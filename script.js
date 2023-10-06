7bb7d80b5695689d908e5eb7e9181459d05b8d32
andrew <andrew_hoffman262@outlook.com>
2023-10-05 23:42:52 -0600

    finishing html document

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelectorSelector("body").appendChild(h2);
+    const sadCatImage = document.getElementById('sad-cat-image');
+
+    sadCatImage.addEventListener('click', function() {
+      sadCatImage.src = 'https://media.istockphoto.com/id/1267021092/photo/funny-winking-kitten.jpg?s=612x612&w=0&k=20&c=9PoFYkqKZ30F_ubxX90_azwsR22ENwrFnOjxV0RaoTo=';
+    });
